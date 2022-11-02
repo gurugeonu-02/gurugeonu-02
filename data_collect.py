2c9dd02929322a75cd32d023228f8a98bf6b3d5b
gurugeonu-02 <114560649+gurugeonu-02@users.noreply.github.com>
2022-11-02 15:49:54 +0900

    Update asodfaiosdjfioasjdfviojasogvbjasdfgmn

--- data_collect.py
+++ data_collect.py
@@ -6,9 +6,11 @@ filename = "datas.csv"
 
 for i in range(1000, 25904):
     page = requests.get("https://www.acmicpc.net/problem/"+str(i))
-    if page.status_code != 200:
-        continue
+    if page.status_code != 200: continue;
     soup = bs(page.text, "html.parser")
     val = soup.find_all('td')
-    print(val)
+    val[2]# 제출
+    val[3]# 정답
+    val[4]# 맞힌 사람
+    val[5]# 정답 비율
     #012345 => X X 제출 정답 맞힌사람 정답비율
